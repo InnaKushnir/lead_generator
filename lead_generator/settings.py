489cccbaceefe1fec3911cfb8941946a303daa6c
Inna Kushnir <innakushnir63@gmail.com>
2023-07-03 14:36:59 +0300

    Solution

--- lead_generator/settings.py
+++ lead_generator/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-LOGIN_REDIRECT_URL = "/home/"
+LOGIN_REDIRECT_URL = "/"
 
 STATIC_URL = "static/"
 
