c0d2a012ead468cedc74bd65bd0855b8f081bbc7
Inna Kushnir <innakushnir63@gmail.com>
2023-07-03 14:05:52 +0300

    Solution

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from app.views import UserCreateView, home, ObjectListView, ObjectCreateView
 
 urlpatterns = [
-    path("home/", home, name="home"),
+    path("", home, name="home"),
     path("user/create/", UserCreateView.as_view(), name="user-create"),
     path("object/", ObjectListView.as_view(), name="object-list"),
     path("object/create/", ObjectCreateView.as_view(), name="object-create"),
